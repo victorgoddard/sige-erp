d0a97f489b8fd1835ab928470a5692692f80535c
Victor <victorg17@live.com>
2026-03-02 00:31:11 -0300

    first commit

---
+++ backend/src/test/java/com/sige_erp/sige_erp/SigeErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sige_erp.sige_erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigeErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
