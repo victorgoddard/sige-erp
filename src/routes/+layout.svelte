173d0a0e8739e955b7b85c52f05e5b0648a4d1a3
YagoArruda <yago4444@gmail.com>
2026-05-21 19:21:55 -0300

    Ajuste de elementos indevidos

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -23,14 +23,14 @@
 
 <div class="bg-white h-screen">
   <div class="drawer lg:drawer-open">
-  <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
+  <!--<input id="my-drawer-4" type="checkbox" class="drawer-toggle" />-->
   <div class="drawer-content">
-    <nav class="navbar w-full bg-base-300">
+    <!--<nav class="navbar w-full bg-base-300">
       <label for="my-drawer-4" aria-label="open sidebar" class="btn btn-square btn-ghost">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor" class="my-1.5 inline-block size-4"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M9 4v16"></path><path d="M14 10l2 2l-2 2"></path></svg>
       </label>
       <div class="px-4">KnowNote</div>
-    </nav>
+    </nav>-->
     <div class="px-4 sm:px-8 md:px-12 lg:px-14 xl:px-16 2xl:px-20 overflow-y-auto bg-base-100 h-screen">
       {@render children?.()}
     </div>
@@ -39,7 +39,7 @@
   <div class="drawer-side is-drawer-close:overflow-visible">
     <label for="my-drawer-4" aria-label="close sidebar" class="drawer-overlay"></label>
     <div class="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-14 is-drawer-open:w-64">
-      <ul class="menu w-full grow">
+      <!--<ul class="menu w-full grow">
         {#each telas as { urlAtt, label, Icon }}
           <li>
             <a href="{urlAtt}" class="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip={label}>
@@ -48,11 +48,11 @@
             </a>
           </li>
         {/each}
-      </ul>
+      </ul>-->
     </div>
   </div>
 </div>
-</div>
+</div> 
 
 <style>
   @keyframes gradientMove {
